a387205e5dfb335354f82faa9bd4d5a7dff88112
Aeveron <33118787+Aeveron@users.noreply.github.com>
2018-09-27 14:48:36 +0200

    x
    x

--- Program.cs
+++ Program.cs
@@ -5,12 +5,28 @@ using System.Threading;
 namespace ConsoleStars
 {
     class Program
-    {
+    {      
         static void Main(string[] args)
         {
+            var random = new Random();
             var stars = new Star[]
             {
-                new Star(5, 10, 1, 0, 0),
+                new Star(5, 10, 1, 2, 0),
+                new Star(10, 5, 2, 1, 0),
+                new Star(25, 10, 3, 0, 1),
+                new Star(52, 10, 4, -1, 0),
+                new Star(12, 5, 5, 1, 1),
+                new Star(5, 10, 1, 2, 0),
+                new Star(10, 5, 2, 1, 0),
+                new Star(25, 10, 3, 0, 1),
+                new Star(52, 10, 4, -1, 0),
+                new Star(12, 5, 5, 1, 1),
+                new Star(5, 10, 1, 2, 0),
+                new Star(10, 5, 2, 1, 0),
+                new Star(25, 10, 3, 0, 1),
+                new Star(52, 10, 4, -1, 0),
+                new Star(12, 5, 5, 1, 1),
+                new Star(5, 10, 1, 2, 0),
                 new Star(10, 5, 2, 1, 0),
                 new Star(25, 10, 3, 0, 1),
                 new Star(52, 10, 4, -1, 0),
@@ -26,8 +42,7 @@ namespace ConsoleStars
                 }
                 Console.CursorLeft = 0;
                 Console.CursorTop = 0;
-                Thread.Sleep(200);
-                var x = "y";
+                Thread.Sleep(100);
             }
         }
     }
--- Star.cs
+++ Star.cs
@@ -24,15 +24,47 @@ namespace ConsoleStars
         {
             Console.CursorLeft = _x;
             Console.CursorTop = _y;
-            Console.Write(_phaseChars[_phase]);
+            Console.Write(_phaseChars[_phase]);                      
         }
 
         public void Update()
         {
+            var windowWidth = Console.WindowWidth;
+            var windowHeight = Console.WindowHeight;
+            //var _AllowedLength = 0;
             _phase++;
             if (_phase == _phaseChars.Length) _phase = 0;
+            if (_x <= 1)
+            {
+                _speedX = 1;
+            }
+            if (_x >= windowWidth -1)
+            {
+                _speedX = -1;
+            }
+
+            if (_y <= 1)
+            {
+                _speedY = 1;
+            }
+            if (_y >= windowHeight - 1)
+            {
+                _speedY = -1;
+            }
+
             _x += _speedX;
             _y += _speedY;
+
+            //if (_AllowedLength == 0 && _speedX == 0)
+            //{
+            //    for (var i = 0; i < _speedX; i++)
+            //    {
+            //        _speedX--;
+            //        _speedX = 1;
+            //    }
+                
+            //}
+     
         }
     }
 }
