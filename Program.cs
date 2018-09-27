025bb0c77890bb97e4101017eba27bd920235c40
Aeveron <33118787+Aeveron@users.noreply.github.com>
2018-09-27 13:19:02 +0200

    x
    x

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+
+namespace ConsoleStars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stars = new Star[]
+            {
+                new Star(5, 10, 1, 0, 0),
+                new Star(10, 5, 2, 1, 0),
+                new Star(25, 10, 3, 0, 1),
+                new Star(52, 10, 4, -1, 0),
+                new Star(12, 5, 5, 1, 1),
+            };
+            while (true)
+            {
+                Console.Clear();
+                foreach (var star in stars)
+                {
+                    star.Show();
+                    star.Update();
+                }
+                Console.CursorLeft = 0;
+                Console.CursorTop = 0;
+                Thread.Sleep(200);
+                var x = "y";
+            }
+        }
+    }
+}
---
+++ Star.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleStars
+{
+    public class Star
+    {
+        private const string _phaseChars = " .x*x.";
+        private int _x;
+        private int _y;
+        private int _speedX;
+        private int _speedY;
+        private int _phase;
+
+        public Star(int x, int y, int phase, int speedX, int speedY)
+        {
+            _x = x;
+            _y = y;
+            _phase = phase;
+            _speedX = speedX;
+            _speedY = speedY;
+        }
+
+        public void Show()
+        {
+            Console.CursorLeft = _x;
+            Console.CursorTop = _y;
+            Console.Write(_phaseChars[_phase]);
+        }
+
+        public void Update()
+        {
+            _phase++;
+            if (_phase == _phaseChars.Length) _phase = 0;
+            _x += _speedX;
+            _y += _speedY;
+        }
+    }
+}
